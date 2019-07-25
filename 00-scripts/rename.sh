a1439662ad370aaffffa3a093e59f5d0f6484c1f
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-25 17:00:27 +0200

    rename files

---
+++ 00-scripts/rename.sh
@@ -0,0 +1,19 @@
+## rename file from demultiplexing from barcode to indivudal tag
+## name of the file will be compatible with dDocent workflow
+
+#argument 1 : path of the results 04-all_samples/species
+#argument 2 : path of the infos files 01-info_files/species_infos.csv
+
+mkdir $1
+awk 'NR>1 { print $1"\t"$2"\t"$3"\t"$4}' $2 | while read LANE BARCODE POP SAMPLE ;
+do
+	FILE_SE=`ls $(pwd)/03-samples/$LANE | grep "sample_"$BARCODE".fq.gz"`
+ 	if [ -z "$FILE_SE" ]
+	then
+		echo $FILE_SE" is empty"
+	else
+		PATH_SE=$(pwd)/03-samples/$LANE/$FOLDER_RUN/$FILE_SE
+		echo $PATH_SE
+		echo $1"/"$NAME_IND".F.fq.gz"
+	fi
+done
