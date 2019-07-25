279a5d36b244acb6603192310d285dc9093960a7
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-25 17:07:31 +0200

    rename files

--- 00-scripts/rename.sh
+++ 00-scripts/rename.sh
@@ -4,7 +4,7 @@
 #argument 1 : path of the results 04-all_samples/species
 #argument 2 : path of the infos files 01-info_files/species_infos.csv
 
-mkdir $1
+mkdir -p 04-renamed/$1
 awk 'NR>1 { print $1"\t"$2"\t"$3"\t"$4}' $2 | while read LANE BARCODE POP SAMPLE ;
 do
 	FILE_SE=`ls $(pwd)/03-samples/$LANE | grep "sample_"$BARCODE".fq.gz"`
@@ -13,7 +13,8 @@ do
 		echo $FILE_SE" is empty"
 	else
 		PATH_SE=$(pwd)/03-samples/$LANE/$FILE_SE
+		SAMPLE_EDIT=`echo $SAMPLE | sed 's/_/i/g'`
 		echo $PATH_SE
-		echo $1"/"$POP"_"$SAMPLE".F.fq.gz"
+		echo $1"/"$POP"_"$SAMPLE_EDIT".F.fq.gz"
 	fi
 done
