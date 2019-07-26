aa4d84d3e786d497416ed5a9f1d284162224d18a
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-26 15:54:55 +0200

    barcode file generation

--- 00-scripts/barcodes.sh
+++ 00-scripts/barcodes.sh
@@ -1,8 +1,8 @@
 ## create barcodes file for each run
 SPECIES=$1
 
-for fastqf in `awk 'NR>1 { print $1 }' 01-infos/"${SPECIES}"_sample_information.csv | sort | uniq`
+for fastqf in `awk 'NR>1 { print $1 }' 01-infos/"${SPECIES}"_sample_information.tsv | sort | uniq`
 do
-	grep $fastqf 01-infos/"${SPECIES}"_sample_information.csv | awk '{ print $2}' > 01-infos/barcodes/"${fastqf}".txt
+	grep $fastqf 01-infos/"${SPECIES}"_sample_information.tsv | awk '{ print $2}' > 01-infos/barcodes/"${fastqf}".txt
 done
 
