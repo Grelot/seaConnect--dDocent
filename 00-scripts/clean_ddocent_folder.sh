088ca0a226d87db7ae3e290f9e14c1a32b79bc1e
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-08-12 13:35:20 +0200

    clean ddocent temp files

---
+++ 00-scripts/clean_ddocent_folder.sh
@@ -0,0 +1,28 @@
+cd 04-ddocent/mullus/
+rm reference.fasta.a*
+rm reference.fasta.*
+rm *RG.bam*
+rm *unique.seqs
+rm *uniq.seqs
+rm *R1.fq.gz
+rm *cov.stats
+rm uniq.*
+rm TotalRawSNPs.vcf 
+rm totalover.fasta 
+rm -Rf raw.vcf/
+rm totaluniqseq.gz 
+rm -Rf trim_reports/
+rm popmap 
+rm namelist 
+rm -Rf logfiles/
+rm genome.file 
+rm fastp.*
+rm dDocent*
+rm cov.split.stats 
+rm Final.recode.vcf 
+rm freebayes.error 
+rm bamlist.list
+rm nohup.out 
+rm mapped.bed 
+rm uniqCperindv 
+cd ../../
