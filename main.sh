b1f4d01c785b6d65edd9120e289c1b6b24644852
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-08-13 15:17:19 +0200

    set global variable

--- main.sh
+++ main.sh
@@ -1,10 +1,19 @@
+#########################################################################
+### set global variables
+
 ## species to process
 
 #SPECIES=$1
 SPECIES=mullus
 #SPECIES=diplodus
 
-
+## dDocent
+CONTAINER=/entrepot/working/seaconnect/seaConnect--dDocent/seaconnect.simg
+DDOCENT_CONFIG=/entrepot/working/seaconnect/seaConnect--dDocent/01-infos/ddocent_config.file
+CONTAINER=/media/bigvol/eboulanger/seaconnect/seaconnect.simg
+DDOCENT_CONFIG=/media/bigvol/eboulanger/seaconnect/01-infos/ddocent_config.file
+#########################################################################
+### RUN THE WORKFLOW
 
 ### create barcode files
 bash 00-scripts barcodes.sh "${SPECIES}"
@@ -25,16 +34,7 @@ bash 00-script/demort.sh "${SPECIES}"
 bash 00-scripts/rename.sh "${SPECIES}" 01-infos/"${SPECIES}"_sample_information.tsv 98-metrics/"${SPECIES}"_samples_blacklist.txt
 
 
-
-## dDocent
-CONTAINER=/entrepot/working/seaconnect/seaConnect--dDocent/seaconnect.simg
-DDOCENT_CONFIG=/entrepot/working/seaconnect/seaConnect--dDocent/01-infos/ddocent_config.file
-CONTAINER=/media/bigvol/eboulanger/seaconnect/seaconnect.simg
-DDOCENT_CONFIG=/media/bigvol/eboulanger/seaconnect/01-infos/ddocent_config.file
-
-
-
-#### add a reference
+#### add a reference genome fasta file to ddocent folder
 ln -s /entrepot/donnees/genomes/"${SPECIES}"_genome.fasta 04-ddocent/"${SPECIES}"/reference.fasta
 #### run the workflow "dDocent"
 bash 00-scripts/ddocent.sh "${SPECIES}" "${CONTAINER}" "${DDOCENT_CONFIG}"
